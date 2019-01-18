48eac1a70bb20a7526397b33ab96e0c93f751160
tuzongxun <1160569243@qq.com>
2019-01-18 14:23:19 +0800

    增加系统日志

--- src/main/java/cn/tzxcode/pay/controller/TzxWapPayController.java
+++ src/main/java/cn/tzxcode/pay/controller/TzxWapPayController.java
@@ -2,6 +2,7 @@ package cn.tzxcode.pay.controller;
 
 import cn.tzxcode.pay.dto.MerchantReqDTO;
 import cn.tzxcode.pay.service.AliPayService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *wap支付统一对外接口
- *@auth tuzongxun
- *@email 1160569243@qq.com
- *@blog https://blog.tzxcode.cn、https://blog.csdn.net/tuzongxun
+ * wap支付统一对外接口
+ *
+ * @auth tuzongxun
+ * @email 1160569243@qq.com
+ * @blog https://blog.tzxcode.cn、https://blog.csdn.net/tuzongxun
  */
 @Controller
 @RequestMapping("/tzxpay")
+@Slf4j
 public class TzxWapPayController {
 
     @Autowired
@@ -24,12 +27,14 @@ public class TzxWapPayController {
 
     /**
      * wap支付接口
+     *
      * @param response
      */
     @GetMapping("/wapPay")
-    public void wapPay(HttpServletResponse response){
-        MerchantReqDTO merchantReqDTO=new MerchantReqDTO();
-        aliPayService.payToAli(response,merchantReqDTO);
+    public void wapPay(HttpServletResponse response) {
+        MerchantReqDTO merchantReqDTO = new MerchantReqDTO();
+        log.info("TzxWapPayController|wapPay|开始处理wap支付请求:" + merchantReqDTO);
+        aliPayService.payToAli(response, merchantReqDTO);
     }
 
 }
--- src/main/java/cn/tzxcode/pay/service/AliPayService.java
+++ src/main/java/cn/tzxcode/pay/service/AliPayService.java
@@ -42,70 +42,109 @@ public class AliPayService {
     public String payToAli(HttpServletResponse response, MerchantReqDTO merchantReqDTO) {
         try {
             //获取支付配置信息
-            PayConfigEntity payConfigEntity=payConfigDao.findPayConfig(merchantReqDTO.getMerchantId(),
-                    merchantReqDTO.getPayType(),merchantReqDTO.getChannel());
-
+            PayConfigEntity payConfigEntity = payConfigDao.findPayConfig(merchantReqDTO.getMerchantId(),
+                    merchantReqDTO.getPayType(), merchantReqDTO.getChannel());
+            log.info("AliPayService|payToAli|支付配置信息：" + payConfigEntity);
             // 封装请求支付信息content
             AliPayReqContentDTO bizContent = new AliPayReqContentDTO();
-            bizContent.setOut_trade_no(merchantReqDTO.getOut_trade_no());
-            bizContent.setSeller_id(payConfigEntity.getSeller_id());
-            bizContent.setTotal_amount(merchantReqDTO.getTotal_amount());
-            bizContent.setSubject(merchantReqDTO.getSubject());
-            bizContent.setSellerEmail(payConfigEntity.getSellerEmail());
-            bizContent.setTimeout_express(payConfigEntity.getTimeout_express());
-            bizContent.setBody(merchantReqDTO.getSubject());
-            bizContent.setProduct_code(payConfigEntity.getProduct_code());
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(payConfigEntity.getGateWay());
-
+            assembleBizContent(merchantReqDTO, payConfigEntity, bizContent);
             String bizStr = JSONObject.toJSONString(bizContent);
             //封装待签名请求信息
             AliPayReqDTO aliPayReqDTO = new AliPayReqDTO();
-            aliPayReqDTO.setApp_id(payConfigEntity.getAppId());
-            aliPayReqDTO.setMethod(payConfigEntity.getMethod());
-            aliPayReqDTO.setCharset(payConfigEntity.getCharset());
-            aliPayReqDTO.setTimestamp(merchantReqDTO.getTimestamp());
-            aliPayReqDTO.setVersion(payConfigEntity.getVersion());
-            aliPayReqDTO.setNotify_url(payConfigEntity.getNotifyUrl());
-            aliPayReqDTO.setBiz_content(bizStr);
-            aliPayReqDTO.setSign_type(payConfigEntity.getSign_type());
+            assembleSignParam(merchantReqDTO, payConfigEntity, bizStr, aliPayReqDTO);
             //待签名参数排序
-            String reqStr = AliSignUtil.orderParamsMapAndReturnParamsString(MapUtil.objectToMap(aliPayReqDTO));
+            String reqStr =
+                    AliSignUtil.orderParamsMapAndReturnParamsString(MapUtil.objectToMap(aliPayReqDTO));
+            log.info("AliPayService|payToAli|待签名信息：" + reqStr);
             //rsa2签名加密
-            String sign = AliSignUtil.signRsa2(reqStr, payConfigEntity.getPrivateKey(), payConfigEntity.getCharset());
-
-            //组装表单方式的请求参数
-            List<NameValuePair> content1 = new ArrayList<NameValuePair>();
-            content1.add(new BasicNameValuePair("app_id", payConfigEntity.getAppId()));
-            content1.add(new BasicNameValuePair("method", payConfigEntity.getMethod()));
-            content1.add(new BasicNameValuePair("charset", payConfigEntity.getCharset()));
-            content1.add(new BasicNameValuePair("timestamp", merchantReqDTO.getTimestamp()));
-            content1.add(new BasicNameValuePair("version", payConfigEntity.getVersion()));
-            content1.add(new BasicNameValuePair("biz_content", bizStr));
-            content1.add(new BasicNameValuePair("sign_type", payConfigEntity.getSign_type()));
-            content1.add(new BasicNameValuePair("notify_url", payConfigEntity.getNotifyUrl()));
-            content1.add(new BasicNameValuePair("sign", sign));
-            System.out.println(content1.toString());
-            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(content1, "UTF-8");
-            post.setEntity(entity);
+            String sign = AliSignUtil.signRsa2(reqStr, payConfigEntity.getPrivateKey(),
+                    payConfigEntity.getCharset());
+            log.info("AliPayService|payToAli|签名信息：" + sign);
 
             //发起支付请求
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(payConfigEntity.getGateWay());
+            //组装表单方式的请求参数
+            List<NameValuePair> content1 = new ArrayList<NameValuePair>();
+            assembleFormParam(merchantReqDTO, payConfigEntity, bizStr, sign, content1);
+            log.info("AliPayService|payToAli|发送支付宝请求数据：" + content1.toString());
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(content1, "UTF-8");
+            post.setEntity(entity);
             HttpResponse res = httpClient.execute(post);
             if ("alipay.trade.wap.pay".equals(payConfigEntity.getMethod())) {
-                //    支付宝wap支付直接跳转到支付宝支付页面
+                // 支付宝wap支付直接跳转到支付宝支付页面
                 Header locationHeader = res.getFirstHeader("Location");
                 String location = locationHeader.getValue();
-                System.out.println(location);
+                log.info("AliPayService|payToAli|支付响应跳转地址：" + location);
                 response.sendRedirect(location);
                 return "pay success";
             }
             return "pay fail";
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error("AliPayService|payToAli|支付异常：" + e);
         }
-        //return res.getBody();
         return "pay fail";
+    }
+
+    /**
+     * 组装支付请求表单参数
+     *
+     * @param merchantReqDTO
+     * @param payConfigEntity
+     * @param bizStr
+     * @param sign
+     * @param content1
+     */
+    private void assembleFormParam(MerchantReqDTO merchantReqDTO, PayConfigEntity payConfigEntity,
+                                   String bizStr, String sign, List<NameValuePair> content1) {
+        content1.add(new BasicNameValuePair("app_id", payConfigEntity.getAppId()));
+        content1.add(new BasicNameValuePair("method", payConfigEntity.getMethod()));
+        content1.add(new BasicNameValuePair("charset", payConfigEntity.getCharset()));
+        content1.add(new BasicNameValuePair("timestamp", merchantReqDTO.getTimestamp()));
+        content1.add(new BasicNameValuePair("version", payConfigEntity.getVersion()));
+        content1.add(new BasicNameValuePair("biz_content", bizStr));
+        content1.add(new BasicNameValuePair("sign_type", payConfigEntity.getSign_type()));
+        content1.add(new BasicNameValuePair("notify_url", payConfigEntity.getNotifyUrl()));
+        content1.add(new BasicNameValuePair("sign", sign));
+    }
+
+    /**
+     * 组装签名数据
+     *
+     * @param merchantReqDTO
+     * @param payConfigEntity
+     * @param bizStr
+     * @param aliPayReqDTO
+     */
+    private void assembleSignParam(MerchantReqDTO merchantReqDTO, PayConfigEntity payConfigEntity,
+                                   String bizStr, AliPayReqDTO aliPayReqDTO) {
+        aliPayReqDTO.setApp_id(payConfigEntity.getAppId());
+        aliPayReqDTO.setMethod(payConfigEntity.getMethod());
+        aliPayReqDTO.setCharset(payConfigEntity.getCharset());
+        aliPayReqDTO.setTimestamp(merchantReqDTO.getTimestamp());
+        aliPayReqDTO.setVersion(payConfigEntity.getVersion());
+        aliPayReqDTO.setNotify_url(payConfigEntity.getNotifyUrl());
+        aliPayReqDTO.setBiz_content(bizStr);
+        aliPayReqDTO.setSign_type(payConfigEntity.getSign_type());
+    }
+
+    /**
+     * 组装支付请求报文体
+     *
+     * @param merchantReqDTO
+     * @param payConfigEntity
+     * @param bizContent
+     */
+    private void assembleBizContent(MerchantReqDTO merchantReqDTO, PayConfigEntity payConfigEntity,
+                                    AliPayReqContentDTO bizContent) {
+        bizContent.setOut_trade_no(merchantReqDTO.getOut_trade_no());
+        bizContent.setSeller_id(payConfigEntity.getSeller_id());
+        bizContent.setTotal_amount(merchantReqDTO.getTotal_amount());
+        bizContent.setSubject(merchantReqDTO.getSubject());
+        bizContent.setSellerEmail(payConfigEntity.getSellerEmail());
+        bizContent.setTimeout_express(payConfigEntity.getTimeout_express());
+        bizContent.setBody(merchantReqDTO.getSubject());
+        bizContent.setProduct_code(payConfigEntity.getProduct_code());
     }
 
 }
